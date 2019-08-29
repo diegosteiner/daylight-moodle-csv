5a15f2bd4e21490ba18d03b968ac9232ad11b91b
Diego Steiner <diego.steiner@u041.ch>
2019-08-29 13:55:27 +0200

    Improve

--- src/index.js
+++ src/index.js
@@ -6,9 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const courseInput = document.querySelector('#course-input')
   const passwordInput = document.querySelector('#password-input')
   const eventInput = document.querySelector('#event-input')
+  const mailInput = document.querySelector('#mail-input')
 
   csvHandler.setup(csvInput, csvOutput, courseInput, passwordInput)
-  // eventInput.addEventListener('change', (ev) => {
+  courseInput.addEventListener('change', (ev) => {
+    const courseName = courseInput.value
+    const password = passwordInput.value
+    const text = `
+Liebe
 
-  // })
+Dein Moodle-Kurs für das Modul ${courseName} steht dir ab sofort unter [LINK] zur Verfügung. Das Standardpasswort
+für die Teilnehmenden lautet ${password}
+
+Freundliche Grüsse
+          `
+    mailInput.value = text
+
+  })
 })
